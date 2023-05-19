dc26cdd1110994facf2739ba8353052ea69532f8
Danica-Kane <100318768+Danica-Kane@users.noreply.github.com>
2023-05-19 20:46:50 +1200

    final flask set up

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+#add pages here
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/templates/new_page.html')
+def new_page():
+    return render_template('new_page.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
