22a0c9eeaa8106aedc8eeb632b9b7abd1dd9f348
Danica-Kane <100318768+Danica-Kane@users.noreply.github.com>
2023-05-22 21:21:03 +1200

    nav bar draft

--- static/script.js
+++ static/script.js
@@ -11,3 +11,21 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
   }))
+
+  var acsessibility = document.getElementById("acsessibility-btn");
+
+  acsessibility.onclick = function() {
+  acsessibility.style.background = "#00BF63";  
+}
+
+const dyslexia = document.querySelector(".dyslexia-btn");
+
+dyslexia.addEventListener("click", () => {
+  document.body.classList.toggle('dyslexia');
+  })
+
+  const fontSize = document.querySelector(".font-size-btn");
+
+fontSize.addEventListener("click", () => {
+  document.body.classList.toggle('font-size');
+  })
