0b17828274f618f6d937692e4ed0238d2c692c12
Danica-Kane <100318768+Danica-Kane@users.noreply.github.com>
2023-05-22 13:22:30 +1200

    nav bar

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, url_for
-from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__, static_url_path='/static')
-bootstrap = Bootstrap5(app)
 
 #add pages here
 @app.route('/', methods=['GET', 'POST'])
--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+/* NAV BAR HAMBURGER */
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -10,3 +11,23 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
   }))
+
+  /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
