e1249d55109f28c495ac057e8e2fed17cad6041f
Danica-Kane <100318768+Danica-Kane@users.noreply.github.com>
2023-05-29 21:21:30 +1200

    home page

--- static/script.js
+++ static/script.js
@@ -3,40 +3,60 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  })
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-  }))
+  })
+);
 
-  var acsessibility = document.getElementById("acsessibility-btn");
+/* ACSESSIBILITY */
 
-  acsessibility.onclick = function() {
-  acsessibility.style.background = "#00BF63";  
-}
+var acsessibility = document.getElementById("acsessibility-btn");
+
+acsessibility.onclick = function () {
+  acsessibility.style.background = "#00BF63";
+};
 
 const dyslexia = document.querySelector(".dyslexia-btn");
 
 dyslexia.addEventListener("click", () => {
-  document.body.classList.toggle('dyslexia');
-  })
+  document.body.classList.toggle("dyslexia");
+});
 
-  dyslexia.addEventListener("click", () => {
-    var element = document.getElementById("home-text-one");
-    element.classList.toggle("colum-home-1-text-font");
-    })
+/* remove classes */
 
-  const fontSize = document.querySelector(".font-size-btn");
+dyslexia.addEventListener("click", () => {
+  var element = document.getElementById("home-text-one");
+  element.classList.toggle("colum-home-1-text-font");
+});
+
+dyslexia.addEventListener("click", () => {
+  var element = document.getElementById("home-text-two");
+  element.classList.toggle("colum-home-2-text-font");
+});
+
+dyslexia.addEventListener("click", () => {
+  var element = document.getElementById("h1");
+  element.classList.toggle("home-header-one-font");
+});
+
+dyslexia.addEventListener("click", () => {
+  var element = document.getElementById("h1-2");
+  element.classList.toggle("home-header-two-font");
+});
+
+const fontSize = document.querySelector(".font-size-btn");
 
 fontSize.addEventListener("click", () => {
-  document.body.classList.toggle('font-size');
-  })
-
+  document.body.classList.toggle("font-size");
+});
 
 /* --- PARRALAX SCROLL --- */
 
-var image = document.getElementsByClassName('hero-banner');
+var image = document.getElementsByClassName("hero-banner");
 new simpleParallax(image);
