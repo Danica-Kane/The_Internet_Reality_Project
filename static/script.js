c230a7d77df0ee1e3cea3fe098e0184f39825d5d
Danica-Kane <100318768+Danica-Kane@users.noreply.github.com>
2023-07-17 16:11:58 +1200

    v3

--- static/script.js
+++ static/script.js
@@ -75,7 +75,7 @@ if (mode2 == 'sizeRegular') {
 var image = document.getElementsByClassName("hero-banner");
 new simpleParallax(image);
 
-/* -- GAME -- */
+/* --- GAME --- */
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
