a11bbc7ef16972aa220dde5d98087fdffe5b54a2
Danica-Kane <100318768+Danica-Kane@users.noreply.github.com>
2023-07-31 12:39:38 +1200

    v3

--- api/app.py
+++ api/app.py
@@ -1,14 +1,12 @@
+# IMPORT FLASK AND FLASK MAIL
 from flask import Flask, redirect, render_template, request, url_for
 from flask_mail import Mail, Message
 
-
-#-------------
-
+# SET UP INITAL RENDER COMANDS
 app = Flask(__name__, static_url_path='/static')
-
 mail = Mail(app) 
 
-# configuration of mail
+# CONFIGERATION OF MAIL
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'internetrealiltyproject@gmail.com'
@@ -17,11 +15,9 @@ app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
-#--------
-
 subscribers = []
 
-#add pages here
+# RENDER PAGES
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
@@ -41,12 +37,14 @@ def Game():
 
 @app.route('/api/templates/form.html', methods=['POST'])
 def form():
+    # get form contents
     email_adress = request.form.get("email_adress")
     subject_line = request.form.get("subject_line")
     your_message = request.form.get("your_message")
 
     subscribers.append(f"{email_adress} | {subject_line} | {your_message}")
 
+    # send form contents to email recipients
     if request.method == "POST":
         msg = Message(
                 subject_line,
@@ -64,6 +62,7 @@ def form():
     
     return redirect("/api/templates/Contact.html", code=302)
 
+# RUN APP
 if __name__ == "__main__":
     app.run(debug=True)
 
